716c8424d5f4f4ec43ac59d50ad9626aca2ec35f
hilldrupca <hilldrupca@gmail.com>
2020-09-30 19:48:11 -0400

    September 30, 2020 challenge

---
+++ Monthly/Sep2020/firstmissingpositive.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        '''
+        Returns the smallest missing positive integer.
+        
+        Examples:
+            firstMissingPositive([1,2,0])       ->   3
+            firstMissingPositive([3,4,-1,1])    ->   2
+            firstMissingPositive([7,8,9])       ->   1
+            firstMissingPositive([0,2,2,1,1])   ->   3
+        '''
+        nums.sort()
+        lowest = 0
+        for x in nums:
+            if x - lowest > 1:
+                return lowest + 1
+            elif x > 0:
+                lowest = x
+                
+        return lowest + 1

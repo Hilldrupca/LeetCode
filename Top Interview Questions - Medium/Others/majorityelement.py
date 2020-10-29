06efb36d40f6ef422ae38a124b0cb6c3a0892ebe
hilldrupca <hilldrupca@gmail.com>
2020-10-29 15:51:16 -0400

    Problem - majority element

---
+++ Top Interview Questions - Medium/Others/majorityelement.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        '''
+        Returns the element that appears more than len(nums)/2 times.
+        '''
+        nums.sort()
+        return nums[len(nums)//2]

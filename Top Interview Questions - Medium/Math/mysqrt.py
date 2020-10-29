039343a5cbc341d6aa569442e5e41dfc6a8934ea
hilldrupca <hilldrupca@gmail.com>
2020-10-29 15:14:56 -0400

    Problem - sqrt(x)

---
+++ Top Interview Questions - Medium/Math/mysqrt.py
@@ -0,0 +1,8 @@
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        '''
+        Returns the truncated square root of a number. Answer is truncated
+        to the nearest whole number.
+        '''
+        return int(x**.5)

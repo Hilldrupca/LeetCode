8edb7d6d3675fd20fefeadea707ac446ea6fb87a
hilldrupca <hilldrupca@gmail.com>
2020-11-08 16:51:05 -0500

    Added examples to docstring

--- N-ary Tree/preordertraversal.py
+++ N-ary Tree/preordertraversal.py
@@ -8,14 +8,37 @@ class Node:
 class Solution:
     def preorder(self, root: Node) -> List[int]:
         '''
-        Given the root node of an n-ary tree, returns the preorder traversal of
-        the tree.
+        Rturns the preorder traversal of an n-ary tree.
         
         Params:
             root - The root node of an n-ary tree.
             
         Returns:
             List[int] - The preorder traversal of the tree.
+            
+        Examples:
+            Given the following n-ary tree, a:
+                    1
+                   /|\
+                  3 2 4
+                 / \
+                5   6
+                
+            postorder(a)   ->   [1,3,5,6,2,4]
+            
+            Given the following n-ary tree, b:
+                ____1_____
+               /   / \    \
+              /   /   \    \
+             2   3     4    5
+                / \    |   / \
+               6   7   8  9   10
+                   |   |  |
+                  11  12  13
+                   |
+                  14
+                  
+            postorder(b)   ->   [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
         '''
         if root is None:
             return []

e2175c02244a5f36972986195c1c07c0f847fc18
hilldrupca <hilldrupca@gmail.com>
2021-02-20 13:12:20 -0500

    Added documentation

--- rust/Monthly/Feb2021/roman_to_integer/src/main.rs
+++ rust/Monthly/Feb2021/roman_to_integer/src/main.rs
@@ -1,6 +1,34 @@
-struct Solution {}
+pub struct Solution {}
 
+/// LeetCode Monthly Challenge problem for February 20th, 2021
 impl Solution {
+    
+    /// Converts the roman numeral to an integer.
+    ///
+    /// # Arguments
+    ///
+    /// * 's' - A string consisting of the roman numerals I, V, X, L, C, D, and/or M
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let three = String::from("III");
+    /// let four = String::from("IV");
+    /// let nine = String::from("IX");
+    /// let fifty_eight = String::from("LVIII");
+    /// let nineteen_ninety_four = String::from("MCMXCIV");
+    /// 
+    /// assert_eq!(Solution::roman_to_int(three), 3);
+    /// assert_eq!(Solution::roman_to_int(four), 4);
+    /// assert_eq!(Solution::roman_to_int(nine), 9);
+    /// assert_eq!(Solution::roman_to_int(fifty_eight), 58);
+    /// assert_eq!(Solution::roman_to_int(nineteen_ninety_four), 1994);
+    /// ```
+    ///
+    /// # Constraints
+    /// * 1 <= s.len() <= 15
+    /// * It is guaranteed that s is a valid roman numeral in the range [1, 3999]
+    ///
     pub fn roman_to_int(s: String) -> i32 {
         let mut res: Vec<i32> = Vec::new();
         
